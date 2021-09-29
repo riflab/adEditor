c185463b4aa234e831fadd71d12cbed18a0ad2e9
riflab <arif.darmawan@riflab.com>
2021-09-29 08:21:03 +0700

    create pandas database

--- src/read_edi_file.py
+++ src/read_edi_file.py
@@ -1,9 +1,4 @@
-def check_float(val):
-    try:
-        float(val)
-        return True
-    except ValueError:
-        return False
+import pandas as pd
 
 
 def read_edi(file):
@@ -12,18 +7,37 @@ def read_edi(file):
     # ]
 
     list_variable = [
-        '>FREQ', '>ZROT',
+        '>FREQ',
+        '>ZROT',
         '>ZXXR', '>ZXXI', '>ZXX.VAR',
         '>ZXYR', '>ZXYI', '>ZXY.VAR',
         '>ZYXR', '>ZYXI', '>ZYX.VAR',
         '>ZYYR', '>ZYYI', '>ZYY.VAR',
+        '>RHOROT',
+        '>RHOXY', '>RHOXY.ERR', '>RHOXY.FIT',
+        '>RHOYX', '>RHOYX.ERR', '>RHOYX.FIT',
+        '>RHOXX', '>RHOXX.ERR',
+        '>RHOYY', '>RHOYY.ERR',
+        '>PHSXY', '>PHSXY.ERR', '>PHSXY.FIT',
+        '>PHSYX', '>PHSYX.ERR', '>PHSYX.FIT',
+        '>PHSXX', '>PHSXX.ERR',
+        '>PHSYY', '>PHSYY.ERR',
+        '>TROT.EXP',
+        '>TXR.EXP', '>TXI.EXP', '>TXVAR.EXP',
+        '>TYR.EXP', '>TYI.EXP', '>TYVAR.EXP',
+        '>TIPMAG', '>TIPMAG.VAR', '>TIPPHS',
+        '>ZSTRIKE', '>ZSKEW', '>ZELLIP',
+        '>TSTRIKE', '>TSKEW', '>TELLIP',
+        '>INDMAGR.EXP', '>INDMAGI.EXP',
+        '>INDANGR.EXP', '>INDANGI.EXP',
         '>END'
     ]
+    df = pd.DataFrame()
 
     site_name = None
     n_frequency = None
 
-    frequency = []
+    list_data = []
 
     f = open(file, 'r')
     temp = f.readline()
@@ -40,20 +54,23 @@ def read_edi(file):
                 n_frequency = int(a[0].split('=')[1])
 
             if a[0] == list_variable[-1]:
-                print('break', list_variable[ind])
+                # print(list_variable[ind])
                 break
 
             if a[0] == list_variable[ind]:
-                print(list_variable[ind])
-
-                while len(frequency) < n_frequency:
+                while len(list_data) < n_frequency:
                     temp = f.readline()
                     b = temp.split()
-                    frequency += b
-                    print(list_variable[ind], len(frequency), b)
+                    list_data += b
+
+                # print(list_variable[ind], list_data)
+                df[list_variable[ind]] = list_data
                 ind += 1
-                frequency = []
+                list_data = []
 
         temp = f.readline()
 
-    print(site_name, n_frequency, frequency)
+    print(site_name, n_frequency)
+    print(df)
+
+    return df
